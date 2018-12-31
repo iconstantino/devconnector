347a00ad4df4baffe339c6713cd086c2928d1311
Ivan <icaivan11@gmail.com>
2018-12-30 19:46:56 -0800

    Add JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ivan:ic123567@ds145574.mlab.com:45574/devconnector"
+  mongoURI: "mongodb://ivan:ic123567@ds145574.mlab.com:45574/devconnector",
+  secretOrKey: "secret"
 };
